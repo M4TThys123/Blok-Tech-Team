3c082cf5e4b0dc6edb5a31c1f0de73fa84c32ee9
ROEL2407 <roel24komen@gmail.com>
2021-03-16 14:17:42 +0100

    changed route and filename of icebreaker feature

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use(express.static('static'));
 app.engine('handlebars', exphbs());
 app.set("view engine", 'handlebars');
 
-app.get('/', async (req, res) => {
+app.get('/q&a', async (req, res) => {
   var vragen = [];
   //takes all the questions from the database and places them into the array vragen
   vragen = await db.collection('questions').find({}).toArray();
@@ -57,10 +57,10 @@ app.get('/', async (req, res) => {
       randVraag.push(vraagHolder);
     }
   }
-  res.render('home', {randVraag});
+  res.render('questions', {randVraag});
 });
 
-app.post('/', async (req,res) => {
+app.post('/q&a', async (req,res) => {
   //pushes chosen answers to the database with the id's from the users
   const questAndAnswer = {"person1": person[0].id, "ansPerson1": req.body.answer, "person2": person[1].id, "ansPerson2": req.body.answer};
   console.log(req.body.answer);
@@ -71,7 +71,7 @@ app.post('/', async (req,res) => {
 }).catch(function(error){
     res.send(error);
 })
-  res.render('home', {questAndAnswer});
+  res.render('questions', {questAndAnswer});
 });
 
 
