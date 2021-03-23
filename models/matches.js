5d70cd4c7fbffc5e85e8183b1c9ff553a36d8d50
marloestacx <marloes.tacx@live.nl>
2021-03-23 16:37:20 +0100

    required velden toegevoegd

--- models/matches.js
+++ models/matches.js
@@ -2,11 +2,10 @@ const mongoose = require('mongoose');
 const schemeName = 'matches';
 
 const profielSchema = new mongoose.Schema({
-    id: { type: Number},
-    person1: { type: String },
-    ansPerson1: { type: String},
-    person2: { type: String},
-    ansPerson2: { type: String},
+    person1: { type: String, required: true },
+    ansPerson1: { type: String, required: true},
+    person2: { type: String, required: true},
+    ansPerson2: { type: String, required: true},
 },{collection:'matches'});
 
 module.exports = mongoose.model(schemeName, profielSchema)
--- models/vragen.js
+++ models/vragen.js
@@ -5,7 +5,7 @@ const voorkeurSchema = new mongoose.Schema({
     id: { type: Number},
     vraag: { type: String, required: true },
     ant1: { type: String, required: true },
-    ant2: { type: String,required: true},
+    ant2: { type: String, required: true},
 },{collection:'vragen'});
 
 module.exports = mongoose.model(schemeName, voorkeurSchema)
